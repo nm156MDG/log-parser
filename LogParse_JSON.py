91a7c1ecc67996c580c47d6259e4bf0baff02a77
nm156MDG <mike.garonnm156@gmail.com>
2016-08-30 14:57:55 -0400

    Correcting file structure.
    Removed duplicate files.

---
+++ LogParse_JSON.py
@@ -0,0 +1,12 @@
+# Build out functions to parse JSON logs.
+
+import os
+import json
+
+# Still not working.
+# dir_location = 'c:/Users/mgaron/documents/CatOneTempData'
+# json_list = "c:/Users/mgaron/documents/CatOneTempData/CATONE_CAT1000269_20160510_110322_SUMMARY.json"
+# json_dict = json.load(json_list)
+# print = json_dict[json_list]
+
+
---
+++ test_functions.py
@@ -0,0 +1,6 @@
+# This will be the unit test workspace.
+
+import unittest
+
+class TestMethod(unittest.TestCase):
+
